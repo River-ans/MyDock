cd6c61406386152aa8cfd6660adf4ca58888c496
River-ans <mriveroom@gmail.com>
2024-12-16 22:33:44 +0900

    chore: 프로젝트 초기화 및 기존 코드 제거
    - 기존에 개발된 모든 코드와 파일 삭제
    - 프로젝트를 초기 상태로 재설정
    - 새로운 작업을 위한 준비 상태로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-'use client';
-
 import React from 'react';
-import { SearchForm } from '@/components/shared/SearchForm';
-import { Favorites } from '@/components/shared/Favorites';
-import { FavoritesForm } from '@/components/shared/FavoritesForm';
 
 export default function Home() {
   return (
-    <div className='w-full h-full flex flex-col items-center justify-center'>
-      <SearchForm />
-      <Favorites />
-      <FavoritesForm />
-    </div>
+    <div className='w-full h-full flex flex-col items-center justify-center'></div>
   );
 }
