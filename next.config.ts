52000ed7b87f2eff43d4d58b53c0bd92d2d6f18f
River-ans <mriveroom@gmail.com>
2024-12-19 21:56:30 +0900

    feat:SearchInput 컴포넌트  분리

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: undefined, // Turbopack 비활성화
-  },
-  webpack(config) {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'], // Webpack 로더 설정
+      use: ['@svgr/webpack'],
     });
     return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
   },
 };
 
---
+++ src/components/shared/searchBox/SearchInput.tsx
@@ -0,0 +1,20 @@
+import SearchIcon from '@/assets/icons/search.svg';
+export function SearchInput() {
+  return (
+    <div
+      className='w-96 h-12 flex items-center bg-primary-800/70 p-1 rounded-xl
+      border border-primary-200/50 shadow-xl shadow-primary-800/40'
+    >
+      <div className='px-2'>
+        <SearchIcon width={18} height={18} className='text-primary-200/80' />
+      </div>
+      <input
+        type='text'
+        placeholder='search'
+        className='bg-transparent text-primary-100 font-bold
+          placeholder:text-primary-200/80    placeholder:text-lg
+          placeholder:font-bold focus:outline-none'
+      />
+    </div>
+  );
+}
